42be0800d2fd1832e88f27c479ed3d992d63e38b
ByDSA <danisales.es@gmail.com>
2025-02-24 18:42:15 +0100

    chore: actualizadas la mayoría de dependencias

--- packages/ai/jest.config.js
+++ packages/ai/jest.config.js
@@ -6,16 +6,9 @@ module.exports = {
   roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": require.resolve("babel-jest"),
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  transformIgnorePatterns: ["<rootDir>/node_modules/?!(@datune)"],
-  globals: {
-    "ts-jest": {
-      isolatedModules: false,
-    },
-  },
   moduleNameMapper: {
     "^tests($|/.*)$": "<rootDir>/tests/$1",
   },
--- packages/utils/jest.config.js
+++ packages/utils/jest.config.js
@@ -5,17 +5,10 @@ module.exports = {
   ],
   roots: ["<rootDir>/src"],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": require.resolve("babel-jest"),
+    "^.+\\.tsx?$": "ts-jest"
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  transformIgnorePatterns: ["<rootDir>/node_modules/?!(@datune)"],
-  globals: {
-    "ts-jest": {
-      isolatedModules: false,
-    },
-  },
   moduleNameMapper: {
     "^@time/(.*)$": "<rootDir>/src/time/$1",
     "^@tests/(.*)$": "<rootDir>/tests/$1",
