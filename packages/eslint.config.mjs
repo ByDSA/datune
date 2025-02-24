369d58757cefa04c33752858dcbafa9615da53d6
ByDSA <danisales.es@gmail.com>
2025-02-24 23:17:12 +0100

    chore: añadir prettier, aplicar fix, mejorar linting

--- packages/eslint.config.mjs
+++ packages/eslint.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 
+import * as fs from "node:fs";
+import * as path from "node:path";
 import { Dependencies } from "daproj";
 import { generateConfigs } from "daproj/eslint";
 import { configs as projectConfigs } from "./lib/eslint.project.config.mjs";
@@ -10,7 +12,18 @@ const generatedConfigs = generateConfigs( {
   [Dependencies.Prettier]: true,
   [Dependencies.TypeScript]: true,
 } );
+const getSubprojects = (dir) => {
+  return fs.readdirSync(dir).filter(subdir => {
+    const subdirPath = path.join(dir, subdir);
+
+    return fs.statSync(subdirPath).isDirectory() && fs.existsSync(path.join(subdirPath, "package.json"));
+  } );
+};
+const subprojects = getSubprojects(process.cwd());
 const infrastructureConfig = [
+  {
+    ignores: subprojects.map(s=>"**/" + s),
+  },
   {
     files: ["**/*.ts"],
     rules: {
