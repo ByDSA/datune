9e945bbdfb3d4642d424678ff96ca6938f5c4211
ByDSA <danisales.es@gmail.com>
2025-03-09 13:25:35 +0100

    chore: add testing for examples

---
+++ examples/bin/test-errors.mjs
@@ -0,0 +1,25 @@
+#!/usr/bin/env zx
+import path from "node:path";
+
+// eslint-disable-next-line no-undef
+const files = await glob("../**/[0-9][0-9]*.ts");
+
+for (const file of files) {
+  try {
+    console.log(`Executing ${file}...`);
+    const absolutePath = path.resolve(file);
+    const result = await $`npx tsx ${absolutePath}`;
+
+    if (result.exitCode !== 0) {
+      console.error(`Error executing ${file}`);
+      console.error(result.stderr);
+      process.exit(result.exitCode);
+    }
+  } catch (error) {
+    console.error(`Error executing ${file}`);
+    console.error(error.stderr || error.message);
+    process.exit(error.exitCode || 1);
+  }
+}
+
+console.log("All TypeScript files executed successfully");
