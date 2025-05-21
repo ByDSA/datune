689bcf1ff539f535cc63fea0c55aca003aeeef6a
ByDSA <danisales.es@gmail.com>
2025-05-21 12:10:47 +0200

    feat: improve pitch set creation and cache ID generation

--- packages/core/src/sets/pitch-set/chromatic/building/index.ts
+++ packages/core/src/sets/pitch-set/chromatic/building/index.ts
@@ -1,18 +1,9 @@
 import type { PitchSet } from "../PitchSet";
-import type { PitchArray, Pitch } from "pitches/chromatic";
+import type { PitchArray } from "pitches/chromatic";
 import { cache } from "../caching/cache";
 
 export function from(...pitches: PitchArray): PitchSet {
-  const set = pitches2Set(...pitches);
+  const set = new Set(pitches);
 
   return cache.getOrCreate(set);
-}
-
-function pitches2Set(...notes: PitchArray): Set<Pitch> {
-  const set = new Set<Pitch>();
-
-  for (const n of notes)
-    set.add(n);
-
-  return set;
 }
--- packages/core/src/sets/pitch-set/chromatic/caching/cache.ts
+++ packages/core/src/sets/pitch-set/chromatic/caching/cache.ts
@@ -1,39 +1,17 @@
 import type { Pitch } from "pitches/chromatic";
 import { KeyMappedFlyweightCache } from "datils/patterns/caching";
-import { Pitches } from "pitches/chromatic";
 import { PitchSet } from "../PitchSet";
 
-/* eslint-disable @typescript-eslint/naming-convention */
 export type Key = Set<Pitch>;
 
 export const getKey = (ps: PitchSet) => ps.pitches;
 
 export const getId = (key: Key): string => {
-  const { A, AA, B, C, CC, D, DD, E, F, FF, G, GG } = Pitches;
-  const hasC = key.has(C) ? 1 : 0;
-  const hasCC = key.has(CC) ? 1 : 0;
-  const hasD = key.has(D) ? 1 : 0;
-  const hasDD = key.has(DD) ? 1 : 0;
-  const hasE = key.has(E) ? 1 : 0;
-  const hasF = key.has(F) ? 1 : 0;
-  const hasFF = key.has(FF) ? 1 : 0;
-  const hasG = key.has(G) ? 1 : 0;
-  const hasGG = key.has(GG) ? 1 : 0;
-  const hasA = key.has(A) ? 1 : 0;
-  const hasAA = key.has(AA) ? 1 : 0;
-  const hasB = key.has(B) ? 1 : 0;
-  const id = (hasC * 2)
-        + (hasCC * 3)
-        + (hasD * 5)
-        + (hasDD * 7)
-        + (hasE * 11)
-        + (hasF * 13)
-        + (hasFF * 17)
-        + (hasG * 19)
-        + (hasGG * 23)
-        + (hasA * 29)
-        + (hasAA * 31)
-        + (hasB * 37);
+  let id = 0;
+
+  for (const pitch of key)
+    // eslint-disable-next-line no-bitwise
+    id |= 1 << +pitch;
 
   return id.toString();
 };
